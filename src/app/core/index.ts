aba3520431d0361ed997f13cc8cf9eeeae3eb312
Gergely Koncz <gergelykoncz@Gergelys-Mac-mini.local>
2018-11-29 17:13:04 +0100

    CoreModule multiple import prevention

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http-service';
 
@@ -6,6 +6,12 @@ import { HttpService } from './http-service';
   imports: [HttpClientModule]
 })
 export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: CoreModule,
